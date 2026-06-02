5154097ba769fd2b52bbf6ae97a2b3b4a292a002
JessFreak <maliy.olexander@lll.kpi.ua>
2026-06-02 12:48:50 +0300

    deploy(app): change apiUrl in environment.prod.ts ONE MORE TIME

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://134.112.25.236.nip.io:3000/api',
+  apiUrl: '/api',
   imgbbApiKey: '264bd91eb572649c584781cba1489426',
   imgbbApiUrl: 'https://api.imgbb.com/1/upload',
 };
